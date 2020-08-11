ca549c6b120c35733be18db171cf3a07b3a83525
Michael Oldacre <mloldacre@live.com>
2020-08-11 10:14:17 -0400

    JSON exercise

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 
+app.use(morgan('dev'));
+
 app.get('/', (request, response) => {
   response.send('Hello Express!');
 });
@@ -19,15 +21,91 @@ app.get('/pizza/pepperoni', (request, response) => {
   response.send('Your pizza is on the way!');
 });
 
+app.get('/pizza/pineapple', (request, response) => {
+  response.send('We don\'t serve that here. Never call again!');
+});
+
+//Working with the Express response object
+app.get('/hello', (req, res) => {
+  res.status(204).end();
+});
+
+app.get('/video', (req, res) => {
+  const video = {
+    title: 'Cats falling over',
+    description: '15 minutes of hilarious fun as cats fall over',
+    length: '15.40'
+  };
+  res.json(video);
+});
+
+app.get('/colors', (req, res) => {
+  const colors = [
+    {
+      name: "red",
+      rgb: "FF0000"
+    },
+    {
+      name: "green",
+      rgb: "00FF00"
+    },
+    {
+      name: "blue",
+      rgb: "0000FF"
+    },
+  ];
+  res.json(colors);
+});
+
+app.get('/grade', (req, res) => {
+  // get the mark from the query
+  const { mark } = req.query;
+
+  // do some validation
+  if (!mark) {
+    // mark is required
+    return res
+      .status(400)
+      .send('Please provide a mark');
+  }
+
+  const numericMark = parseFloat(mark);
+  if (Number.isNaN(numericMark)) {
+    // mark must be a number
+    return res
+      .status(400)
+      .send('Mark must be a numeric value');
+  }
+
+  if (numericMark < 0 || numericMark > 100) {
+    // mark must be in range 0 to 100
+    return res
+      .status(400)
+      .send('Mark must be in range 0 to 100');
+  }
+
+  if (numericMark >= 90) {
+    return res.send('A');
+  }
+
+  if (numericMark >= 80) {
+    return res.send('B');
+  }
+
+  if (numericMark >= 70) {
+    return res.send('C');
+  }
+
+  res.send('F');
+});
+
+
+//Exercises 8/10/2020
 app.get('/sum', (request, response) => {
   const a = parseInt(request.query.a);
   const b = parseInt(request.query.b);
   const c = a + b;
   response.send(`The sum of ${a} and ${b} is ${c}`);
-});
-
-app.get('/pizza/pineapple', (request, response) => {
-  response.send('We don\'t serve that here. Never call again!');
 });
 
 app.get('/cipher', (request, response) => {
@@ -82,5 +160,5 @@ app.get('/lotto', (request, response) => {
 });
 
 app.listen(8080, () => {
-  console.log('Express server is listening on port 8000!');
+  console.log('Express server is listening on port 8080!');
 });
